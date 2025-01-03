450aaa80811738457b9fc8edfb3c2cf5f3c8efcd
user <rumapro6@gmail.com>
2025-01-03 09:11:59 +0900

    2025.01.03 Form 변수 전달 안되는 원인 확인 중 / 개발 중 / 2025.01.03 최신화

--- btd6_CT_map/src/test/java/com/rumadev/btd6ctmap/config/NotConnectDB.java
+++
@@ -1,9 +0,0 @@
-package com.rumadev.btd6ctmap.config;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class NotConnectDB {
-
-}
